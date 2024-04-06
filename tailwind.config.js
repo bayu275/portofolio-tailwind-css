f9de24f5dfa095d495ab0bb3854ee7be68599e7c
Bayu <bayuusetiawan86@gmail.com>
2024-04-06 12:56:50 +0700

    feat(UI): :lipstick: add home UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
     content: ['index.html'],
     theme: {
-        extend: {},
+        container: {
+            center: true,
+            padding: '16px',
+        },
+        extend: {
+            colors: {
+                primary: '#14b8a6',
+                primary_alt: '#2dd4bf',
+                secondary: '#64748b',
+                dark: '#0f172a',
+            },
+            screens: {
+                '2xl': '1320px',
+            },
+        },
     },
     plugins: [],
 };
